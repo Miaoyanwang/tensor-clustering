b63a436fd9fdb4932baa04828d650785a360d1ed
Wang lab <saplingwang@gmail.com>
2019-03-01 00:40:47 -0600

    update

--- code/plot.R
+++ code/plot.R
@@ -1,17 +1,23 @@
 library(rgl)
 library("RColorBrewer")
-marker = list(color = brewer.pal(9, "RdBu"))
+library(viridis) ## use more refined color scale
+##marker = list(color = brewer.pal(9, "RdBu"))$color
 
 ### function to plot 3d array
 plot_tensor=function(tensor){
     
+n=prod(dim(tensor))   
+color_choice=min(round(prod(dim(tensor))/(6*6*6)),100)+1
+marker=viridis_pal(option = "B")(color_choice)
+
 position=positionfun(dim(tensor))$position
-quan=c(quantile(tensor,(0:8)/8),max(tensor))
+quan=c(quantile(tensor,(0:color_choice)/color_choice))
 col=tensor
-for(i in 1:9){
-col[(tensor>=quan[i])&(tensor<quan[i+1])]=marker$color[i]
+for(i in 1:color_choice){
+col[(tensor>=quan[i])&(tensor<quan[i+1])]=marker[i]
 }
-    
+col[tensor==quan[i+1]]=marker[i]
+
 plot3d(position[,1],position[,2],position[,3],col=col,alpha=0.3,size=5,xlab="",ylab="",zlab="")
 }
 
