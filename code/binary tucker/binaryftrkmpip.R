59c7431f46ff129ee7b170bb2ac4b07e6156ac2d
Marchhu36 <43443433+Marchhu36@users.noreply.github.com>
2019-03-16 22:55:08 -0500

    Add files via upload

--- code/binary tucker/binaryftrkmpip.R
+++ code/binary tucker/binaryftrkmpip.R
@@ -8,62 +8,121 @@ source("upgrade_new.R")
 ######packages#######
 library(R.matlab)
 library(rTensor)
-library(tensorr)
+library(tensr)
 
-binary_tensor_fctr_km = function(tensor,k,maxiter){#k is for cp factorization 
+binary_tensor_fctr_km = function(tensor,k,maxiter){
+  #k is number for cp factorization 
   #and k can be a vector is we use trucker decomposition
   ##########replace NaN by 0######
   tensor[is.na(tensor)] = 0
   
   #########find the initial matrices#########
+  #if use cp
   #########take the elements as continuous variables#######
   #######zoom y to 10*(2y-1)#######
-  tensor_i = tensor
-  tensor_i = as.tensor(10*(2*tensor_i-1))
+  # tensor_i = tensor
+  # tensor_i = as.tensor(10*(2*tensor_i-1))
   
   ######The model would be#######
   ######logit(p_ijk) = C x M1 x M2 x M3
   ######initialize C,M1,M2,M3#######
-  ###here use cp factorization#####
-  inital_dep = cp(tnsr = tensor_i,num_components = k)
-  M1_0 = as.matrix(inital_dep$U[[1]])
-  M2_0 = as.matrix(inital_dep$U[[2]])
-  M3_0 = as.matrix(inital_dep$U[[3]])
-  ##initial C would be identity tensor###
-  C_0 = matrix(0,nrow = k^3)
-  dim(C_0) = c(k,k,k)
-  for (i in 1:k) {####special initialize for cp factorization
-    C_0[i,i,i] =1
-  }
+
+  # inital_dep = cp(tnsr = tensor_i,num_components = k)
+  # M1_0 = as.matrix(inital_dep$U[[1]])
+  # M2_0 = as.matrix(inital_dep$U[[2]])
+  # M3_0 = as.matrix(inital_dep$U[[3]])
+  # ##initial C would be identity tensor###
+  # C_0 = matrix(0,nrow = k^3)
+  # dim(C_0) = c(k,k,k)
+  # for (i in 1:k) {####special initialize for cp factorization
+  #   C_0[i,i,i] =1
+  # }
+  # 
   
-  likelihood = rep(0,maxiter)
-  
-  likelihood[1] = cal_likelihood(tensor,C_0,M1_0,M2_0,M3_0)
+  #here use tucker
+  inital_tucker = tucker(as.tensor(tensor),ranks = k)
+  C_0 = inital_tucker$Z
+  M1_0 = as.matrix(inital_tucker$U[[1]])
+  M2_0 = as.matrix(inital_tucker$U[[2]])
+  M3_0 = as.matrix(inital_tucker$U[[3]])
+  likelihood = cal_likelihood(tensor,C_0,M1_0,M2_0,M3_0)
   modes = list("U"=C_0,"M1"=M1_0,"M2" =M2_0,"M3"=M3_0)
   
+  mode_problem = NULL
   for (iter in 2:maxiter) {
     modes_new = mode_upgrade_glm(tensor,modes$U,modes$M1,modes$M2,modes$M3)
-    likelihood[iter] = cal_likelihood(tensor,modes_new$U,modes_new$M1
-                                      ,modes_new$M2,modes_new$M3)
+    likelihood = c(likelihood,modes_new$likelihood)
     print(iter)
-    if(likelihood[iter]<=likelihood[iter-1]){
-      likelihood = likelihood[1:iter]
+    if(likelihood[(1+4*(iter-1))]<=likelihood[(4*(iter-2)+1)]){
+      #compare the full upgrade likelihood
+      mode_problem = modes_new
+      #record the problem matrices and core tensor
+      #and the problem matrices and core tensor for each step can be 
+      #found by mode_problem$mode_p
+      #which record the problem matries and tensor if the likelihood is inf
       break
     }
-    
     modes = modes_new
   }
   
-  M1_km = kmeans(modes$M1,centers = k)#if use trucker, there would be k1
-  M2_km = kmeans(modes$M2,centers = k)
-  M3_km = kmeans(modes$M3,centers = k)
+  M1_km = kmeans(modes$M1,centers = k[1])#if use trucker, there would be k1
+  M2_km = kmeans(modes$M2,centers = k[2])
+  M3_km = kmeans(modes$M3,centers = k[3])
   
   return(list("modes"=modes,"M1_CL" = M1_km$cluster,"M2_CL" = M2_km$cluster
-              ,"M3_CL" = M3_km$cluster,"likelihood"=likelihood))
+              ,"M3_CL" = M3_km$cluster,"likelihood"=likelihood
+              ,"mode_problem" = mode_problem))
 }
 
 dnoations = readMat("dnations.mat")
 dnoations_tr = dnoations$R
-test  =binary_tensor_fctr_km(dnoations_tr,5,10)
+test_222  =binary_tensor_fctr_km(dnoations_tr,c(2,2,2),50)
+test_223  =binary_tensor_fctr_km(dnoations_tr,c(2,2,3),50)
+test_333  =binary_tensor_fctr_km(dnoations_tr,c(3,3,3),50)
+test = binary_tensor_fctr_km(dnoations_tr,c(3,3,4),50)
+
+
+###why likelihood would drop to negative infinity???
+problem = test$mode_problem$mode_p$p3#p3 means the third upgrading step sth wrong
+cor(problem$M2)#the third step corresponds to upgrade M2 or B in our document
+
+###如果更新m3的时候出了问题 检验更新m3的predictor是不是相关
+
+# UM1 = amprod(problem$U, problem$M1, 1)
+# UM1M2 = amprod(UM1,problem$M2,2)
+# 
+# x3 = UM1M2[1,,]
+# for (a in 2:d1) {
+#   x3 = rbind(x3,UM1M2[a,,])
+# }
+# cor(x3)
+# dim(x3)
+# cal_likelihood(tensor,problem$U,problem$M1,problem$M2,M3_y)
+# 
+# M3_y = matrix(0,nrow = d3,ncol = 4)
+# for (i in 1:d3) {
+#   y = tensor[1,,i]
+#   for (j in 2:d1) {
+#     y = c(y,tensor[j,,i])
+#   }
+#   gm = glm(y~0+x3,family=binomial(link="logit"),control = list(maxit = 50)
+#            ,start = test$modes$M3[i,])
+#   M3_y[i,]=gm$coefficients
+# }
+
+###如果更新m2的时候出了问题 检验更新m2的predictor是不是相关
+
+UM1 = amprod(problem$U,problem$M1,1)
+UM1M3 = amprod(UM1,problem$M3,3)
+
+x2 = UM1M3[1,,]
+for (a in 2:d1) {
+  x2 = cbind(x2,UM1M3[a,,])
+}
+cor(t(x2)) 
+
+#要转置因为要把tensor切片成矩阵的话 
+#r会默认mode数小的是行，mode数大的是列
+
 
 
--- code/binary tucker/upgrade_new.R
+++ code/binary tucker/upgrade_new.R
@@ -7,12 +7,12 @@ library(R.matlab)
 library(rTensor)
 library(tensr)
 library(tensor)
+library(gtools)
 
 #upgrading functions
 mode_upgrade_glm = function(tensor,U_0,M1_0,M2_0,M3_0){
   #U_0,M1_0,M2_0,M3_0 are initial core tensor and factor matrices
   #M1_0 \in R^{d_1 \times k_1}
-  #class of tensor should be array
   
   #get the dimensions
   k1 = dim(U_0)[1]
@@ -23,12 +23,23 @@ mode_upgrade_glm = function(tensor,U_0,M1_0,M2_0,M3_0){
   d2 = dim(M2_0)[1]
   d3 = dim(M3_0)[1]
   
+  #set likelihood 
+  likelihood = rep(0,4)
+  
+  #find problem matrices
+  mode_p1 = NULL
+  mode_p2 = NULL
+  mode_p3 = NULL
+  mode_p4 = NULL
+
   #upgrade core tensor
   
   #first find y of glm #find predictor
   y = rep(0,d1*d2*d3)
   x = matrix(0,nrow = d1*d2*d3,ncol = k1*k2*k3)
+  
   m=1
+  
   for (i in 1:d1) {
     for (j in 1:d2) {
       for (k in 1:d3) {
@@ -46,8 +57,27 @@ mode_upgrade_glm = function(tensor,U_0,M1_0,M2_0,M3_0){
   # what y looks like
   #[1,1,1],...,[1,1,d3],[1,2,1],...,[1,2,d3],...,[1,d2,d3],
   #[2,1,1],...,[2,1,d3],[1,2,1],...,[1,2,d3],...,[2,d3,d3],...
+  if(is.array(U_0)==FALSE){
+    U_0 = U_0@data #tensor class is a S4 class, need to use @ to get the data
+  }
   
-  glforu = glm(y~0+x,family=binomial(link="logit"),control = list(maxit = 50))
+  if(is.array(U_0)==FALSE){
+    print("not array")
+  }
+  
+  #find the start coefficient
+  start_u = rep(0,k1*k2*k3)
+  a=1
+  for (i in 1:k1) {
+    for (j in 1:k2) {
+      for (k in 1:k3) {
+        start_u[a]= U_0[i,j,k]
+        a = a+1
+      }
+    }
+  }
+  
+  glforu = glm(y~0+x,family=binomial(link="logit"),control = list(maxit = 100))
   glforu_coef = glforu$coefficients
   U = matrix(0,nrow = k1*k2*k3)
   dim(U) = c(k1,k2,k3)
@@ -59,6 +89,32 @@ mode_upgrade_glm = function(tensor,U_0,M1_0,M2_0,M3_0){
         n=n+1
       }
     }
+  }
+  
+  likelihood[1] = cal_likelihood(tensor,U,M1_0,M2_0,M3_0)
+  
+  if(likelihood[1] == -Inf){#if give inf then try another strategy
+    glforu = glm(y~0+x,family=binomial(link="logit"),control = list(maxit = 100)
+                 ,start = start_u)
+    glforu_coef = glforu$coefficients
+    U = matrix(0,nrow = k1*k2*k3)
+    dim(U) = c(k1,k2,k3)
+    n=1
+    for (i in 1:k1) {
+      for (j in 1:k2) {
+        for (k in 1:k3) {
+          U[i,j,k] =  glforu$coefficients[n]
+          n=n+1
+        }
+      }
+    }
+    
+    likelihood[1] = cal_likelihood(tensor,U,M1_0,M2_0,M3_0)
+  }
+  
+  if(likelihood[1] == -Inf){ #after the strategy if the likelihood is still inf
+    print("still inf")
+    mode_p1 = list("U"=U,"M1"=M1_0,"M2" =M2_0,"M3"=M3_0)
   }
   
   ###upgrade M_3###
@@ -73,7 +129,7 @@ mode_upgrade_glm = function(tensor,U_0,M1_0,M2_0,M3_0){
   
   M3 = matrix(0,nrow = d3,ncol = k3) 
   for (i in 1:d3) {
-    y = tensor[1,,3]
+    y = tensor[1,,i]
     for (j in 2:d1) {
       y = c(y,tensor[j,,i])
     }
@@ -81,6 +137,28 @@ mode_upgrade_glm = function(tensor,U_0,M1_0,M2_0,M3_0){
     M3[i,]=gm$coefficients
   }
 
+  likelihood[2] = cal_likelihood(tensor,U,M1_0,M2_0,M3)
+  
+  if(likelihood[2] == -Inf){
+    M3 = matrix(0,nrow = d3,ncol = k3)
+    for (i in 1:d3) {
+      y = tensor[1,,i]
+      for (j in 2:d1) {
+        y = c(y,tensor[j,,i])
+      }
+      gm = glm(y~0+x3,family=binomial(link="logit"),control = list(maxit = 50)
+               ,start = M3_0[i,])
+      M3[i,]=gm$coefficients
+    }
+    
+    likelihood[2] = cal_likelihood(tensor,U,M1_0,M2_0,M3)
+  }
+  
+  if(likelihood[2] == -Inf){
+    print("still inf")
+    mode_p2 = list("U"=U,"M1"=M1_0,"M2" =M2_0,"M3"=M3)
+  }
+  
   ###upgrade M_2###
   
   UM1M3 = amprod(UM1,M3,3)
@@ -98,6 +176,27 @@ mode_upgrade_glm = function(tensor,U_0,M1_0,M2_0,M3_0){
     }
     gm = glm(y~0+t(x2),family=binomial(link="logit"),control = list(maxit = 50))
     M2[i,]=gm$coefficients
+  }
+  
+  likelihood[3] = cal_likelihood(tensor,U,M1_0,M2,M3)
+  
+  if(likelihood[3] == -Inf){
+    M2 = matrix(0,nrow = d2,ncol = k2)
+    for (i in 1:d2) {
+      y = tensor[1,i,]
+      for (j in 2:d1) {
+        y = c(y,tensor[j,i,])
+      }
+      gm = glm(y~0+t(x2),family=binomial(link="logit"),control = list(maxit = 50)
+               ,start = M2_0[i,])
+      M2[i,]=gm$coefficients
+    }
+    likelihood[3] = cal_likelihood(tensor,U,M1_0,M2,M3)
+  }
+  
+  if(likelihood[3] == -Inf){
+    print("still inf")
+    mode_p3 = list("U"=U,"M1"=M1_0,"M2" =M2,"M3"=M3)
   }
   
   ###upgrade M_1###
@@ -121,16 +220,121 @@ mode_upgrade_glm = function(tensor,U_0,M1_0,M2_0,M3_0){
     M1[i,]=gm$coefficients
   }
   
-  return(list("U"=U,"M1"=M1,"M2" =M2,"M3"=M3))
+  likelihood[4] = cal_likelihood(tensor,U,M1,M2,M3)
+  if(likelihood[4] == -Inf){
+    M1 = matrix(0,nrow = d1,ncol = k1)
+
+    for (i in 1:d1) {
+      y = tensor[i,1,]
+      for (j in 2:d2) {
+        y = c(y,tensor[i,j,])
+      }
+      gm = glm(y~0+t(x1),family=binomial(link="logit"),control = list(maxit = 50)
+               ,start = M1_0[i,])
+      M1[i,]=gm$coefficients
+    }
+    
+    likelihood[4] = cal_likelihood(tensor,U,M1,M2,M3)
+  }
+  if(likelihood[4] == -Inf){
+    print("still inf")
+    mode_p4 = list("U"=U,"M1"=M1,"M2" =M2,"M3"=M3)
+  }
+  
+  #,"mode_p" = list("p1" = mode_p1,"p2" = mode_p2,"p3" = mode_p3,"p4" = mode_p4)
+  return(list("U"=U,"M1"=M1,"M2" =M2,"M3"=M3,"likelihood" = likelihood
+              ,"mode_p" = list("p1" = mode_p1,"p2" = mode_p2,"p3" = mode_p3,"p4" = mode_p4)))
 }
 
-cal_likelihood = function(tensor,U,M1,M2,M3){#they should be array or matrix
-  T1 = amprod(U,M1,1)
-  T2 = amprod(T1,M2,2)
-  est_tensor = amprod(T2,M3,3)
-  first_term = innerProd(as.tensor(tensor),as.tensor(est_tensor))
-  second_term = sum(log(1+exp(est_tensor)))
-  like = first_term-second_term
-  
+cal_likelihood = function(tensor,U,M1,M2,M3){
+  if(is.array(U) ==TRUE) U =as.tensor(U)
+  T1 = ttm(U,M1,1)#must use class tensor
+  T2 = ttm(T1,M2,2)
+  est_tensor = ttm(T2,M3,3)
+  #first_term = innerProd(as.tensor(tensor),est_tensor)
+  #second_term = sum(log(1+exp(est_tensor@data)))
+  #like = first_term-second_term
+  like=sum(log(inv.logit((2*tensor-1)*est_tensor@data)))
   return(like)
 }
+
+###### below are the process I tried to find the problem #####
+###### so below are messy and unnecessary contents #######
+###### I even consider whether it is because of the overflow of the computer###
+###### But initialization and correlation are still the main problems ####
+
+# U = problem$p1$U
+# M1=problem$p1$M1
+# M2=problem$p1$M2
+# M3=problem$p1$M3
+# sum(exp(est_tensor@data))#this would be inf
+# 
+# library(Rmpfr)
+# big_est = mpfr(est_tensor@data,precBits = 6)
+# second_term = sum(log(1+exp(big_est)))
+# second_term
+# 
+# ###another way to calculate the likelihood 
+# p = exp(est_tensor@data)/(1+exp(est_tensor@data))
+# p
+# first_term1 = innerProd(as.tensor(tensor),as.tensor(p))
+# #不太行 exp（e+13）真的会inf
+# 
+# ###check correlation###
+# d1 = 14 
+# d2 = 14
+# d3 = 56
+# k1 = 3
+# k2 = 3
+# k3 = 5
+# y = rep(0,d1*d2*d3)
+# x = matrix(0,nrow = d1*d2*d3,ncol = k1*k2*k3)
+# 
+# m=1
+# 
+# for (i in 1:d1) {
+#   for (j in 1:d2) {
+#     for (k in 1:d3) {
+#       y[m] = tensor[i,j,k]
+#       x[m,] =kronecker(kronecker(mode_t$M1[i,],mode_t$M2[j,]),mode_t$M3[k,]) 
+#       m = m+1
+#     }
+#   }
+# }
+# dim(x)
+# cor(x)#the correlation of x is not very big
+# 
+# glforu = glm(y~0+x,family=binomial(link="logit"),control = list(maxit = 100))
+# logLik(glforu)
+# 
+# ######搞不定了 我要尝试initialize了
+# #上一个u
+# 
+# start_u = rep(0,k1*k2*k3)
+# n=1
+# for (i in 1:k1) {
+#   for (j in 1:k2) {
+#     for (k in 1:k3) {
+#       start_u[n] = mode_t$U[i,j,k] 
+#       n=n+1
+#     }
+#   }
+# }
+# 
+# glforu0 = glm(y~0+x,family=binomial(link="logit"),control = list(maxit = 100)
+#               ,start = start_u)
+# 
+# u_new = matrix(0,nrow = k1*k2*k3)
+# dim(u_new) = c(k1,k2,k3)
+# n=1
+# for (i in 1:k1) {
+#   for (j in 1:k2) {
+#     for (k in 1:k3) {
+#       u_new[i,j,k] =  glforu0$coefficients[n]
+#       n=n+1
+#     }
+#   }
+# }
+# 
+# cal_likelihood(tensor,U , mode_t$M1,mode_t$M2,mode_t$M3)
+# cal_likelihood(tensor,u_new , M1,M2,M3)#是initialize 的问题
