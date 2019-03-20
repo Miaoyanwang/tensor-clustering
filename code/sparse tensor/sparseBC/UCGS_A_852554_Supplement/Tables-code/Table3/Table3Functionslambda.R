677d70da36c26fd950a904b9979991235f4604ca
Miaoyanwang <saplingwang@gmail.com>
2019-03-19 23:22:29 -0500

    Update Table3Functionslambda.R
    update

--- code/sparse tensor/sparseBC/UCGS_A_852554_Supplement/Tables-code/Table3/Table3Functionslambda.R
+++ code/sparse tensor/sparseBC/UCGS_A_852554_Supplement/Tables-code/Table3/Table3Functionslambda.R
@@ -18,41 +18,26 @@ selectedlambda<-c(rep(NA,iteration))
 
 
 
-  for(iter in 1:iteration){
-      cat("Iteration",iter,fill=TRUE)
-      set.seed(iter)
-      mus<-runif(k*r,-2,2)
-      mus<-matrix(c(mus),nrow=k,ncol=r,byrow=FALSE)
-      truthCs<-sample(1:k,n,rep=TRUE)
-      truthDs<-sample(1:r,p,rep=TRUE)
-      x<-matrix(rnorm(n*p,mean=0,sd=4),nrow=n,ncol=p)
-      
-      # This is used to calculate the sparsity error rate
-      truthmatrix<-matrix(NA,nrow=n,ncol=p)
-      for(i in 1:max(truthCs)){
-          for(j in 1:max(truthDs)){
-              x[truthCs==i,truthDs==j]<-x[truthCs==i,truthDs==j]+mus[i,j]
-              truthmatrix[truthCs==i,truthDs==j]<-mus[i,j]
-          } 
-      }    
-      binaryX<-(truthmatrix!=0)*1
-      
-      x<-x-mean(x)
-      
-      selectlambda<-sparseBC.BIC(x,k,r,lambda)$lambda
-      selectedlambda[iter]<-selectlambda
-      biclustering<-sparseBC(x,k,r,selectlambda)
-      resmatrix<-(abs(biclustering$mus)>1e-6)
-      cerrow[iter]<-1-adjustedRand(truthCs,biclustering$Cs,randMethod=c("Rand"))
-      cercol[iter]<-1-adjustedRand(truthDs,biclustering$Ds,randMethod=c("Rand"))
-      
-      
-      totalzero[iter]<-sum(resmatrix==0)/(n*p)
-      correctzero[iter]<-sum(resmatrix[which(binaryX==0)]==0)/sum(binaryX==0)
-      correctone[iter]<-sum(resmatrix[which(binaryX==1)]==1)/(n*p-sum(binaryX==0))
-      totalincorrect[iter]<-sum(abs(resmatrix-binaryX))/(n*p)
-      
-  }
+for(iter in 1:iteration){
+cat("Iteration",iter,fill=TRUE)
+set.seed(iter)
+    mus<-runif(k*r,-2,2)
+    mus<-matrix(c(mus),nrow=k,ncol=r,byrow=FALSE)
+    truthCs<-sample(1:k,n,rep=TRUE)
+    truthDs<-sample(1:r,p,rep=TRUE)
+    x<-matrix(rnorm(n*p,mean=0,sd=4),nrow=n,ncol=p)
+
+# This is used to calculate the sparsity error rate
+  truthmatrix<-matrix(NA,nrow=n,ncol=p)
+  for(i in 1:max(truthCs)){
+	for(j in 1:max(truthDs)){
+	  x[truthCs==i,truthDs==j]<-x[truthCs==i,truthDs==j]+mus[i,j]
+      truthmatrix[truthCs==i,truthDs==j]<-mus[i,j]
+	} 
+  }	
+  binaryX<-(truthmatrix!=0)*1
+	
+  x<-x-mean(x)
   
 	  selectlambda<-sparseBC.BIC(x,k,r,lambda)$lambda
 	  selectedlambda[iter]<-selectlambda
