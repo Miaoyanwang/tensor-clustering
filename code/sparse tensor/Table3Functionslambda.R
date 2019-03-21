d455f7ecfdf8d9cab8fd213dc5ac6fb3f667e9cc
Yuchen Zeng <46949490+reaperConan@users.noreply.github.com>
2019-03-20 23:35:25 -0500

    Add files via upload

---
+++ code/sparse tensor/Table3Functionslambda.R
@@ -0,0 +1,57 @@
+#####################################################
+# Table 3 - Evaluation of biclustering methods
+# The mean matrix is not sparse 
+# Simulation for biclustering method with lambda automatically chosen as in Section 5.2
+#####################################################
+require("sparseBCnew")
+library(clues)
+
+Do<-function(n,p,k,r,iteration,lambda,standarddeviation=4,center=FALSE){
+# Initialize some variables	
+cerrow<-c(rep(NA,iteration))	
+cercol<-c(rep(NA,iteration))
+totalzero<-c(rep(NA,iteration))
+correctzero<-c(rep(NA,iteration))
+correctone<-c(rep(NA,iteration))
+totalincorrect<-c(rep(NA,iteration))
+selectedlambda<-c(rep(NA,iteration))
+
+
+
+for(iter in 1:iteration){
+cat("Iteration",iter,fill=TRUE)
+set.seed(iter)
+    mus<-runif(k*r,-2,2)
+    mus<-matrix(c(mus),nrow=k,ncol=r,byrow=FALSE)
+    truthCs<-sample(1:k,n,rep=TRUE)
+    truthDs<-sample(1:r,p,rep=TRUE)
+    x<-matrix(rnorm(n*p,mean=0,sd=4),nrow=n,ncol=p)
+
+# This is used to calculate the sparsity error rate
+  truthmatrix<-matrix(NA,nrow=n,ncol=p)
+  for(i in 1:max(truthCs)){
+	for(j in 1:max(truthDs)){
+	  x[truthCs==i,truthDs==j]<-x[truthCs==i,truthDs==j]+mus[i,j]
+      truthmatrix[truthCs==i,truthDs==j]<-mus[i,j]
+	} 
+  }	
+  binaryX<-(truthmatrix!=0)*1
+	
+  if(center=TRUE) x<-x-mean(x)
+  
+	  selectlambda<-sparseBC.BIC(x,k,r,lambda)$lambda
+	  selectedlambda[iter]<-selectlambda
+      biclustering<-sparseBC(x,k,r,selectlambda)
+      resmatrix<-(abs(biclustering$mus)>1e-6)
+      cerrow[iter]<-1-adjustedRand(truthCs,biclustering$Cs,randMethod=c("Rand"))
+      cercol[iter]<-1-adjustedRand(truthDs,biclustering$Ds,randMethod=c("Rand"))
+  
+    
+totalzero[iter]<-sum(resmatrix==0)/(n*p)
+correctzero[iter]<-sum(resmatrix[which(binaryX==0)]==0)/sum(binaryX==0)
+correctone[iter]<-sum(resmatrix[which(binaryX==1)]==1)/(n*p-sum(binaryX==0))
+totalincorrect[iter]<-sum(abs(resmatrix-binaryX))/(n*p)
+
+}
+return(list(cerrow=c(mean(cerrow),sd(cerrow)/sqrt(iteration)),cercol=c(mean(cercol),sd(cercol)/sqrt(iteration)),totalzero=c(mean(totalzero),sd(totalzero)/sqrt(iteration)),correctzero=c(mean(correctzero),sd(correctzero)/sqrt(iteration)),correctone=c(mean(correctone),sd(correctone)/sqrt(iteration)),totalincorrect=c(mean(totalincorrect),sd(totalincorrect)/sqrt(iteration)),selectedlambda=selectedlambda))	
+}
---
+++ code/sparse tensor/Table3n200p200lambda.R
@@ -0,0 +1,20 @@
+#########################
+# Run AutoBIC
+#########################
+
+install.packages("sparseBCnew_1.2.tar.gz", repos=NULL, type="source")#restart R after running this line
+rm(list = ls())
+source("Table3Functionslambda.R")
+
+
+n=200
+p=200
+sd=4
+iter=50
+
+lambda<-c(0,50,100,200,300,400,500,600,700,800,900,1000,1100,1200)
+bicluster5<-Do(n=n,p=p,k=4,r=5,iteration=iter,lambda=lambda,standarddeviation=sd,center=FALSE)
+
+tab<-rbind(c("Method","rowCER","colCER","CorrectZero","CorrectNonZero","Sparsity","SparsityError"),c(paste("Biclustering Auto Lambda",round(mean(bicluster5$selectedlambda),digits=4),sep=","),paste(round(bicluster5$cerrow[1],digits=4),round(bicluster5$cerrow[2],digits=4),sep=","),paste(round(bicluster5$cercol[1],digits=4),round(bicluster5$cercol[2],digits=4),sep=","),paste(round(bicluster5$correctzero[1],digits=4),round(bicluster5$correctzero[2],digits=4),sep=","),paste(round(bicluster5$correctone[1],digits=4),round(bicluster5$correctone[2],digits=4),sep=","),paste(round(bicluster5$totalzero[1],digits=4),round(bicluster5$totalzero[2],digits=4),sep=","),paste(round(bicluster5$totalincorrect[1],digits=4),round(bicluster5$totalincorrect[2],digits=4),sep=",")))
+
+write(tab,file="autobicn200p200.txt",sep=",")
