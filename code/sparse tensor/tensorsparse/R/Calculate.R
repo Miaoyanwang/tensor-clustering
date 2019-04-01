7e845078a7323e2f394489fc699ca7ea23dcd852
Miaoyanwang <saplingwang@gmail.com>
2019-04-01 01:39:00 -0500

    update

---
+++ code/sparse tensor/tensorsparse/R/Calculate.R
@@ -0,0 +1,27 @@
+#' calculate the correct rate of the result of sparse.choosekrl()
+#' 
+#' calculate the correct rate of the result of sparse.choosekrl()
+#' @param true true k, r, l
+#' @param results result returned by sparse.choosekrl()
+#' 
+#' 
+Calculate<-function(true,results){
+  real<-matrix(true,ncol=3)
+  percent<-0
+  for(i in 1:length(results)){
+    if(nrow(results[[i]])>1){
+      for(a in 1:nrow(results[[i]])){
+        #  	  cat("iteration",a,fill=TRUE)
+        if(sum(results[[i]][a,]==real)==3){
+          percent<-percent+(1/nrow(results[[i]]))
+        }
+      }
+    }
+    else if(nrow(results[[i]])<2){
+      if(sum(results[[i]]==real)==3){
+        percent<-percent+1
+      }	
+    }
+  }
+  return(percent/length(results))
+}
---
+++ code/sparse tensor/tensorsparse/R/sim.choosekrl.R
@@ -0,0 +1,37 @@
+#' simulation: choose the best k,r,l to do clustering when the ranges are given
+#' 
+#' simulation: choose the best k,r,l to do clustering when the ranges are given
+#' @param n the dimension of mode 1
+#' @param p the dimension of mode 2
+#' @param q the dimension of mode 3
+#' @param k true clusters number of mode 1
+#' @param r true clusters number of mode 2
+#' @param l true clusters number of mode 3
+#' @param error noise when producing the data
+#' @param sim.times simulation times
+#' @param method two options: "L0", "L1". Two methods use different penalties, where "L1" indicating Lasso penalty.
+#' @param mode two options: "bic", "crossvalidation".
+#' @return A list of the krl result in different iteration.
+#' 
+
+
+sim.choosekrl <- function(n,p,q,k,r,l,error=1,sim.times=5,method="L0",mode="bic"){
+  #n=20;p=40;q=30;k=2;r=4;l=3
+  classification<-list()
+  if(mode == "bic"){
+    for(a in 1:sim.times){
+      cat("Starting", a, fill=TRUE)
+      x = get.data(n,p,q,k,r,l,error=error)$x
+      classification[[a]]<-choosekrl_bic(x,k=2:5,r=2:5,l=2:5,method=method)$estimated_krl#estimate clusters
+    }
+  }
+  if(mode == "crossvalidation"){
+    for(a in 1:sim.times){
+      cat("Starting", a, fill=TRUE)
+      x = get.data(n,p,q,k,r,l,error=error)$x
+      classification[[a]]<-sparse.choosekrl(x,k=2:5,r=2:5,l=2:5,method=method)$estimated_krl#estimate clusters
+    }
+  }
+  if(mode!="bic" & mode!="crossvalidation") stop("No such kind of mode:", mode, ".\n")
+  return(classification)
+}
