4af3c92f8f2d848ad56d51be51f62a38cf9bf2cb
ZhuoyanXU_Oliver <43462304+OliverXUZY@users.noreply.github.com>
2019-09-04 23:18:17 -0500

    Update functions_synthesis_all.R

--- code/binary tucker/team/functions_synthesis_all.R
+++ code/binary tucker/team/functions_synthesis_all.R
@@ -498,9 +498,9 @@ sele_rank = function(tsr, X_covar1 = NULL, X_covar2 = NULL, X_covar3 = NULL,
                X_covar3 = X_covar3, Nsim = Nsim, linear = linear, cons = cons)
   
   log_lik = unlist(lapply(seq(length(upp)), function(x) tail(upp[[x]]$lglk,1)))
-  if(is.null(X_covar1)) X_covar1 = diag(d1)
-  if(is.null(X_covar2)) X_covar2 = diag(d2)
-  if(is.null(X_covar3)) X_covar3 = diag(d3)
+  if(is.null(X_covar1)) X_covar1 = whole_shape[1]
+  if(is.null(X_covar2)) X_covar2 = whole_shape[2]
+  if(is.null(X_covar3)) X_covar3 = whole_shape[3]
   
   
   BIC = unlist(lapply(seq(length(rank)), 
